d91ca6f900246ac750bb90be8761235cbe9aeb9a
Alexander <52827777+Alexandr696@users.noreply.github.com>
2021-11-16 09:15:10 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Alexander CV</title>
+<!--         <link rel="stylesheet" href="css/style.css">
+        <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"> -->
+    </head>
+    <body>
+        <?php echo 'что-то' ?>
+    </body>
+</html>
